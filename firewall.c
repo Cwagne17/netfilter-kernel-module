32caa2bdb7186ee0df07a12bf886d36ee7562857
Christopher M Wagner <70287489+Cwagne17@users.noreply.github.com>
2022-11-22 14:24:19 -0500

    add telnet drop

--- firewall.c
+++ firewall.c
@@ -8,21 +8,38 @@
 #include <linux/tcp.h>
 #include <linux/udp.h>
 
-unsigned int hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
-    printk(KERN_DEBUG "Hooked yay!\n");
-    // A: Only block telnet traffic
-    // B: Only block UDP packages on port > 2500
-    // C: Only allow web traffic
-    // D: Only block web traffic from a certain domain, e.g., google.com, and allow all other traffic
-    return NF_ACCEPT;
-}
-
 static struct nf_hook_ops nfho __read_mostly = {
         .pf = PF_INET, // Internet IP Protocol 
         .priority = NF_IP_PRI_FIRST,
         .hooknum = NF_INET_PRE_ROUTING, // capture right after packet is recieved
-        .hook = (nf_hookfn *) hook
+        .hook = (nf_hookfn *) nf_hook
 };
+
+unsigned int nf_hook(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+    struct iphdr *iph;
+	struct udphdr *udph;
+    struct tcphdr *tcph;
+
+	if (!skb)
+		return NF_ACCEPT;
+
+	iph = ip_hdr(skb);
+	if (iph->protocol == IPPROTO_UDP) {
+		udph = udp_hdr(skb);
+	}
+	else if (iph->protocol == IPPROTO_TCP) {
+		tcph = tcp_hdr(skb);
+        if (ntohs(tcph->dest) != 23) { // A: Only block telnet traffic
+            return NF_ACCEPT;
+        }
+	}
+	
+	return NF_DROP;
+
+    // B: Only block UDP packages on port > 2500
+    // C: Only allow web traffic
+    // D: Only block web traffic from a certain domain, e.g., google.com, and allow all other traffic
+}
 
 static int firewall_init(void) {
     nf_register_hook(&nfho);
